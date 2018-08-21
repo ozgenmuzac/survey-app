fd12273f551b24520d7a38e42d011cf98df3620e
ozgen.muzac <ozgen.muzac@gmail.com>
2018-08-21 00:40:39 -0700

    Extract css within its own file

--- static/webpack.base.config.js
+++ static/webpack.base.config.js
@@ -13,21 +13,29 @@ module.exports = {
       filename: "[name]-[hash].js",
     },
 
-    plugins: [
-    ],
-
     module: {
         rules: [
             { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' }, // to transform JSX into JS
             { test: /\.less?$/,
-                use: [
-                    'style-loader',
-                    { loader: 'css-loader', options: { modules: true ,localIdentName: '[name]--[local]--[hash:base64:5]'} },
-                    "less-loader",
-                ]
+              use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: [
+                      {loader: 'css-loader',
+                          options: {
+                              modules: true,
+                              localIdentName: '[name]--[local]--[hash:base64:5]'
+                          }
+                      },
+                      "less-loader",
+                  ],
+              }),
             }
         ],
     },
+
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+    ],
 
     resolve: {
         modules: ['node_modules', 'bower_components'],
