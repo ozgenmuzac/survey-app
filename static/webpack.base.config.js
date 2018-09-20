ba63cd6b34d7c8760f003e84b3d65be7df9fc854
ozgenmuzac <ozgen.muzac@gmail.com>
2018-09-19 22:14:08 -0700

    generate css bundles per js bundles + comments on webpack conf

--- static/webpack.base.config.js
+++ static/webpack.base.config.js
@@ -4,37 +4,45 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     context: __dirname,
 
+    // each entry point should be added here
     entry: {
-      'main_app': './js/src/index.js'
+        'main_app': './js/src/index.js',
     },
 
+    // generates js bundles per each entry point
     output: {
-      path: path.resolve('./js/bundles/'),
-      filename: "[name]-[hash].js",
+        path: path.resolve('./js/bundles/'),
+        filename: "[name]-[hash].js",
     },
 
     module: {
         rules: [
-            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' }, // to transform JSX into JS
+            // for transforming JSX to JS
+            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
+            // for transforming less files to css.
             { test: /\.less?$/,
-              use: ExtractTextPlugin.extract({
-                  fallback: 'style-loader',
-                  use: [
-                      {loader: 'css-loader',
-                          options: {
-                              modules: true,
-                              localIdentName: '[name]--[local]--[hash:base64:5]'
-                          }
-                      },
-                      "less-loader",
-                  ],
-              }),
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                // generates hash for each class to make it available in local component
+                                localIdentName: '[name]--[local]--[hash:base64:5]'
+                            }
+                        },
+                        "less-loader",
+                    ],
+                }),
             }
         ],
     },
 
     plugins: [
-        new ExtractTextPlugin('style.css'),
+        new ExtractTextPlugin(Object.assign({
+            // Using `[name]-[hash] for supporting multiple entry point`
+            filename: "[name]-[hash].css"
+        })),
     ],
 
     resolve: {
